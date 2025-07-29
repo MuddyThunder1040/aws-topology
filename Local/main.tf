8c5fb3f20be58f3585586c2bc38c2c53f62a3bac
MuddyThunder1040 <vishnupardha1040@gmail.com>
2025-07-28 23:06:56 -0700

    Update local_file resource to use relative path for sysinfo.txt

--- Local/main.tf
+++ Local/main.tf
@@ -1,4 +1,5 @@
 resource "local_file" "Zotac" {
-  filename = "/home/vishnu/Code-space/Text-files/sysinfo.txt"
+  filename = "${path.module}/sysinfo.txt"
   content  = "Zotac RTX 5070 Ti amp infinity OC 16GB GDDR7X graphics card"
+
 }
